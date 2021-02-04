5173d21793d91a5b346aa1c9ce83785cfa35e272
Rhys-Maxwell <78513268+Rhys-Maxwell@users.noreply.github.com>
2021-02-05 12:11:19 +1300

    Create 00_MMF_Base.py

---
+++ 00_MMF_Base.py
@@ -0,0 +1,30 @@
+# import statements
+
+
+# functions go here
+
+
+# ***** Main Routine *****
+
+# Set up directories / lists needed to hold date
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket detials
+
+  # Get name (can't be blank)
+
+  # Get age (between 12 and 130)
+
+  # Calculate ticket price
+
+  # Loop to ask for snacks
+
+  # Caluclate snack price
+
+  # Ask for payment method (and apply surcharge if necessary)
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
