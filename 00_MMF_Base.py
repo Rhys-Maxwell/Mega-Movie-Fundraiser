a38fe656974e6f39b32ea4f2659fc7d13f46bb9d
Rhys-Maxwell <78513268+Rhys-Maxwell@users.noreply.github.com>
2021-02-12 11:55:51 +1300

    Update 00_MMF_Base.py

--- 00_MMF_Base.py
+++ 00_MMF_Base.py
@@ -12,7 +12,34 @@
 
 # Loop to get ticket detials
 
-  # Get name (can't be blank)
+name = " "
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    if count < 4:
+        print ("You have {} seats "
+            "left".format (MAX_TICKETS - count))
+
+    # Warns user that only one is left!
+    else:
+            print("*** There is ONE seat left!! ***")
+
+    # Get details...
+
+    # Get name (can't be blank)
+    name = input ("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets. \n"
+          "there are {} places still available"
+          .format(count, MAX_TICKETS - count))
+
+
 
   # Get age (between 12 and 130)
 
